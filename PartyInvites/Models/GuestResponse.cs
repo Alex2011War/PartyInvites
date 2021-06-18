e1bd0e800a800776eaf0743888dd7f8d7fa6e340
alexei.vakulencko@yandex.ru <alexei.vakulencko@yandex.ru>
2021-06-18 16:30:24 +0400

    Добавьте файлы проекта.

---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Azure.KeyVault.Models;
+
+namespace PartyInvites.Models
+{ 
+
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Enter your Name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Enter your Email")]
+        [RegularExpression(".+\\@.+\\..+.", ErrorMessage = "Please enter validate your Email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Enter your Phone")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage = "You will attend party")]
+        public bool ? WillAttend { get; set; }
+    }
+}
---
+++ PartyInvites/Models/Repository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartyInvites.Models
+{
+    public class Repository
+    {
+        public static List<GuestResponse> resposes=new List<GuestResponse>();
+       public static IEnumerable<GuestResponse> Responses
+        {
+            get { return resposes; }
+        }
+
+       public static void AddResponses(GuestResponse response)
+       {
+           resposes.Add(response);
+       }
+    }
+}
